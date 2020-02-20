30055f14d31bd060d2cecfa6382446b34a61be5a
indira singh <lucindhy@gmail.com>
2020-02-19 21:48:31 -0300

    four

---
+++ atividade-1-5.js
@@ -0,0 +1,7 @@
+var user = require("readline-sync")
+
+var num1 = user.question('Insira o primer Numero ')
+var num2 = user.question("Insira o seguinte Numero ")
+var num3 = user.question("Insira o ultimo Numero ")
+
+
